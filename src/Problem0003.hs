f40c55e2b789cfd203413ee70c160e4c03d97b07
Daiji Yamashita <yamashita-daiji@sei.co.jp>
2020-04-13 16:43:21 +0900

    update my prime pick up method

--- src/Problem0003.hs
+++ src/Problem0003.hs
@@ -8,14 +8,21 @@ showAnswer = do
     print $ scanl (+) 0 [1..5]
     print $ take 10 primes
     print $ take 10 primes'
+    putStrLn "Primes Test 01"
+    print $ take 10000 primes
+    putStrLn "Primes Test 02"
+    print $ take 10000 primes'
+
 
 primes = map head $ scanl (\\) [2..] [[p,p+p..] | p <- primes]
             where (\\) = Data.List.Ordered.minus
 
-primes' = [2,3,5] ++ [ x | x <- maybePrimes, isReallyPrime x maybePrimes]
-            where
-                maybePrimes :: [Int]
-                maybePrimes = [ 6*a + b | a <- [1..], b <- [1, 5]]
+primes' = [ x | x <- maybePrimes, isReallyPrime x maybePrimes == True]
 
-                isReallyPrime :: Int -> [Int] -> Bool
-                isReallyPrime n candidates = all (\x -> n `mod` x /= 0) $ takeWhile ((>=) (ceiling $ sqrt $ fromIntegral n)) candidates
+--primes'' = foldl (++) 
+
+maybePrimes :: [Int]
+maybePrimes = [2,3,5] ++ [ 6*a + b | a <- [1..], b <- [1, 5]]
+
+isReallyPrime :: Int -> [Int] -> Bool
+isReallyPrime n candidates = all (\x -> n `mod` x /= 0) $ takeWhile ((>=) (truncate $ sqrt $ fromIntegral n)) candidates
